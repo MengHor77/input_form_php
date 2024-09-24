54ddf69f6557572563d6ec90ade1538ab534cc4c
meng hor <piseth125125@gmail.com>
2024-09-24 19:29:03 +0700

    fix: title

--- update.php
+++ update.php
@@ -63,7 +63,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert to Database</title>
+    <title>update</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.12/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
